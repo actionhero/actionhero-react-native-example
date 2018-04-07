239e100461301322c4285652f6a5895754cb15f9
evan tahler <evantahler@gmail.com>
2018-04-07 12:50:38 -0700

    update packages

--- android/app/src/main/java/com/actionheroreactnativeexample/MainActivity.java
+++ android/app/src/main/java/com/actionheroreactnativeexample/MainActivity.java
@@ -1,4 +1,4 @@
-package com.reactnative;
+package com.actionheroreactnativeexample;
 
 import com.facebook.react.ReactActivity;
 
@@ -10,6 +10,6 @@ public class MainActivity extends ReactActivity {
      */
     @Override
     protected String getMainComponentName() {
-        return "reactnative";
+        return "actionheroReactNativeExample";
     }
 }
